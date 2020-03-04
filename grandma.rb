d3402d9da6afaef5a81d0adac630c1bcee33b175
Ty Raino <mstymetris@yahoo.com>
2020-03-04 19:57:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,10 +13,10 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == phrase.upcase
-      "NO, NOT SINCE 1938!"
+  if phrase == "I LOVE YOU GRANDMA!"
+      "I LOVE YOU TOO PUMPKIN"
     elsif phrase == phrase.upcase
-      "NO, NOT SINCE 1938"
+      "NO, NOT SINCE 1938!"
     else "HUH?! SPEAK UP, SONNY!"
  end
 end

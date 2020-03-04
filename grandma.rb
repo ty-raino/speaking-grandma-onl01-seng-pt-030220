f0fba4f043968ceec12cd5e5d7bb41d21ccb032d
Ty Raino <mstymetris@yahoo.com>
2020-03-04 19:53:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,6 +17,6 @@ def speak_to_grandma(phrase)
     puts  "NO, NOT SINCE 1938"
     elsif phrase == "I LOVE YOU GRANDMA!"
       puts "I LOVE YOU TOO PUMPKIN!"
-    else phrase == phrase.capitalize "HUH?! SPEAK UP, SONNY!"
+    else "HUH?! SPEAK UP, SONNY!"
  end
 end
